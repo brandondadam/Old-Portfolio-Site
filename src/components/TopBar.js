6d94ac210be4c64e05ea04e8394f79d6bd9d9e23
bdadam_enok <brandon.dadam@enok.co>
2019-12-21 22:12:28 -0600

    updated react and fixed tool  bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,37 +3,9 @@ import { NavLink } from "react-router-dom";
 import logo from "../images/logo.svg";
 
 class TopBar extends React.Component {
-	constructor(props) {
-		super(props)
-		
-		this.state = {
-      		hasScroll:false
-    	}
-	}
-	
-	componentDidMount() {
-    	window.addEventListener('scroll', this.hasScroll)
-  	}
-
-  	hasScroll = (event) => {
-		  console.log("Event trigger")
-		const scrollTop = window.pageYOffset;
-
-    	if (scrollTop > 50) {
-			console.log("Scroll");
-      		this.setState({
-        		hasScroll: true
-     		})
-    	} else {
-			console.log("Top");
-      		this.setState({
-        		hasScroll: false
-      	})
-    }
-  }
 	render() {
 		return (
-      		<div className={this.state.hasScroll ? 'scroll top-bar' : 'no-scroll top-bar'}>
+      		<div className='top-bar'>
         		<img src={logo} alt="Brandon D'Adam's Logo" />
         		<nav>
           			<NavLink exact to="/">Hi</NavLink>
